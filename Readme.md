cad15d8fc26e0ae1e32a55b74b9a9d5a4de64a68
lakshya-roonwal <erenpool7214@gmail.com>
2024-03-16 22:54:32 +0530

    Initial Commit : Project Setup

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Color Pellate Generator
---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express');
+const app=express();
+const port =80;
+
+app.get('/',(req,res)=>{res.send('Hello World')})
+
+app.listen(port,(req,res)=>{
+console.log(`Server successfully started on http://localhost:${port}`)
+})
