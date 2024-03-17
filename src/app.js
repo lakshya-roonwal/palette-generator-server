000b50f75b21cf3a1db5cc1a71988edc0e812b4f
lakshya-roonwal <erenpool7214@gmail.com>
2024-03-17 12:46:33 +0530

    refacter : Port No. Change

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app=express();
 const cors=require("cors");
-const port =80;
+const port =process.env.PORT||8000;
 
 app.use(cors({
     origin:process.env.CORS_ORIGIN
