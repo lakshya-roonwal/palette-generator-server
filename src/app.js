731639354beeedd872fad5763e0b6de7c90db185
lakshya-roonwal <erenpool7214@gmail.com>
2024-03-17 12:45:51 +0530

    chore : Added CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app=express();
+const cors=require("cors");
 const port =80;
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN
+}));
+app.use(express.json());
 
 module.exports={app};
