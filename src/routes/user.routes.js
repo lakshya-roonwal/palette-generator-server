a8e1bbbbfff9fb8a22e52de96beeb9e9fa73f9d5
lakshya-roonwal <erenpool7214@gmail.com>
2024-03-27 20:49:58 +0530

    refactor : Added some todos

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,5 +3,6 @@ const { setUser } = require("../controllers/user.controllers");
 const router = express.Router();
 
 router.post("/setuser", setUser);
+// TODO : Have to add delete and update routes 
 
 module.exports = router;
